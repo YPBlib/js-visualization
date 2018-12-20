a87ab3c55920e160a59f85cde39f09bdba5ebd9b
YPBlib <ypb1024@foxmail.com>
2018-12-20 14:18:27 +0800

    改变迭代的控制流

---
+++ treemap/README.md
@@ -0,0 +1,16 @@
+# treemep
+
+给出一系列面积数值，将它们拼合在一个矩形中。
+
+目标是minimiaze max{所有矩形的长宽比（ratio）}。
+
+这里定义ratio不小于1。
+
+<img src="figure4.png">
+
+
+---
+
+<b>reference:</b> 
+
+<i>Bruls M., Huizing K., van Wijk J.J. (2000) Squarified Treemaps. In: de Leeuw W.C., van Liere R. (eds) Data Visualization 2000. Eurographics. Springer, Vienna</i>
--- treemap/main.js
+++ treemap/main.js
@@ -1,13 +1,13 @@
 const horizontal=0;
 const vertical=1;
-let maxlength=1600;
-let drawing  = document.getElementById("trmap")
-let ctx = drawing.getContext("2d");
-ctx.strokeStyle="#000000";
-ctx.lineCap="round";
-ctx.lineJoin="round";
-ctx.linewidth=5;
-ctx.strokeRect(10,10,1000,1000);
+const globalWidth=500;
+const globalHeight=500;
+
+function ratio(width,height)
+{
+    if(width>=height)return width/height;
+    return height/width;
+}
 
 function basicRow(x1,y1,x2,y2,way,data)
 {
@@ -35,7 +35,7 @@ function basicRow(x1,y1,x2,y2,way,data)
         }
         let min=ratio(arr[0]/fix,fix);
         let max=ratio(arr[arr.length-1],fix);
-        if(i==0)
+        if(i===0)
         {
             worst=min;
             continue;
@@ -47,62 +47,66 @@ function basicRow(x1,y1,x2,y2,way,data)
             else worst=max;
         }
     }
-    console.log("i=",i," areaSum= ",areaSum);
     return [i,areaSum];
 }
 
 function basicDraw(x1,y1,x2,y2,data)
 {
-    let len=data["sub"].length;
     let width=x2-x1;
     let height=y2-y1;
     let way;
     if(width>=height)way=vertical;
     else way=horizontal;
-    let tmp=basicRow(x1,y1,x2,y2,(way+1)%2,data);
-    let num=tmp[0];
-    let areaSum=tmp[1];
     let x1_=x1,y1_=y1,x2_,y2_;
-    if(way===vertical)
+
+    while(data["sub"].length)
     {
-        x2_=x1_+ areaSum/height;
-        y2_=y2;
+        let tmp=basicRow(x1,y1,x2,y2,(way+1)%2,data);
+        let num=tmp[0];
+        let areaSum=tmp[1];
 
-        for(let i=0;i<num;++i)
+        if(way===vertical)
         {
-            ctx.strokeRect(x1_,y1_,areaSum/height,(data["sub"][i].sum/areaSum)*height);
-            console.log("areaSum=", areaSum,", height= ", height,", unit_area=",data["sub"][i].sum);
-            y1_+= (data["sub"][i].sum/areaSum)*height;
+            x2_=x1_+ areaSum/height;
+            y2_=y2;
+
+            for(let i=0;i<num+1;++i)
+            {
+                ctx.strokeRect(x1_,y1_,areaSum/height,(data["sub"][i].sum/areaSum)*height);
+                console.log("i=",i,"num=",num,", height= ", height,", unit_area=",data["sub"][i].sum);
+                y1_+= (data["sub"][i].sum/areaSum)*height;
+            }
+
+
+            for(let i=0;i<num;++i)
+            {
+                data["sub"].shift();
+            }
+
         }
-
-
-        for(let i=0;i<num;++i)
+        else if(way===horizontal)
         {
-            data["sub"].shift();
-        }
+            x2_=x2;
+            y2_=y2+areaSum/width;
 
+            for(let i=0;i<num;++i)
+            {
+                ctx.strokeRect(x1_,y1_,(data["sub"][i].sum/areaSum)*width,areaSum/width);
+                x1_+= (data["sub"][i].sum/areaSum)*width;
+            }
+
+            for(let i=0;i<num;++i)
+            {
+                data["sub"].shift();
+            }
+        }
+        way=(way+1)%2;
     }
-    else if(way===horizontal)
-    {
-        x2_=x2;
-        y2_=y2+areaSum/width;
 
-        for(let i=0;i<num;++i)
-        {
-            ctx.strokeRect(x1_,y1_,(data["sub"][i].sum/areaSum)*width,areaSum/width);
-            x1_+= (data["sub"][i].sum/areaSum)*width;
-        }
-
-        for(let i=0;i<num;++i)
-        {
-            data["sub"].shift();
-        }
-    }
 
 
 
 }
-
 
 function  msg(e)
 {
@@ -115,34 +119,23 @@ function  msg(e)
         {
             let someThing = e.target.result;
             let dt=JSON.parse(someThing);
-            basicDraw(0,0,1000,1000,dt);
-        };
+            basicDraw(10,10,globalWidth+10,globalHeight+10,dt);
+        }
         return re;
     }
     reader.onload=lam_1(f);
     reader.readAsText(f);
 }
 
-function ratio(width,height)
-{
-    if(width>=height)return width/height;
-    return height/width;
-}
 
 document.getElementById("select file").addEventListener("change", msg);
-
-
-
-
-
-
-
-
-
-
-
-
-
+let drawing  = document.getElementById("trmap");
+let ctx = drawing.getContext("2d");
+ctx.strokeStyle="#000000";
+ctx.lineCap="round";
+ctx.lineJoin="round";
+ctx.linewidth=5;
+ctx.strokeRect(10,10,globalWidth,globalHeight);
 
 
 
