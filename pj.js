bb4a85f0d23ee178edef3e137df40a7c3c5fa593
YPBlib <ypb1024@foxmail.com>
2019-01-11 17:33:30 +0800

    终于把颜色调对了

---
+++ pj.js
@@ -0,0 +1,26 @@
+let drawing  = document.getElementById("canvas");
+let ctx = drawing.getContext("2d");
+
+
+ctx.fillStyle= 'hsl(0,50%,50%)';ctx.fillRect(0,0,30,30);
+ctx.fillStyle= 'hsl(160,50%,50%)';ctx.fillRect(30,0,30,30);
+ctx.fillStyle= 'hsl(280,50%,50%)';ctx.fillRect(60,0,30,30);
+
+ctx.fillStyle= 'hsl(120,50%,50%)';ctx.fillRect(0,30,30,30);
+ctx.fillStyle= 'hsl(40,50%,50%)';ctx.fillRect(30,30,30,30);
+ctx.fillStyle= 'hsl(200,50%,50%)';ctx.fillRect(60,30,30,30);
+
+ctx.fillStyle= 'hsl(240,50%,50%)';ctx.fillRect(0,60,30,30);
+ctx.fillStyle= 'hsl(80,50%,50%)';ctx.fillRect(30,60,30,30);
+ctx.fillStyle= 'hsl(320,50%,50%)';ctx.fillRect(60,60,30,30);
+
+
+
+
+
+
+
+
+
+
+
